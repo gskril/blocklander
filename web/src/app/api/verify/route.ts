c2987c44a83882cf5451fcca1f63e7922aa732fe
Brenner Spear <blspear@gmail.com>
2023-10-27 15:09:13 -0400

    use beaconchain for nft data url

--- web/src/app/api/verify/route.ts
+++ web/src/app/api/verify/route.ts
@@ -2,12 +2,11 @@ import { NextRequest, NextResponse } from 'next/server'
 import z from 'zod'
 import { recoverAddress } from 'ethers/lib/utils'
 import { hashMessage } from '@ethersproject/hash'
-import { parseSearchParams, generateMintingSignature, Signature } from '@/lib/utils'
+import { parseSearchParams, generateMintingSignature, Signature, fetchBeaconChainData } from '@/lib/utils'
 
 
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || ''
 const TEST_CONTRACT_ADDRESS = process.env.TEST_CONTRACT_ADDRESS || ''
-const BEACONCHAIN_API_KEY = process.env.BEACONCHAIN_API_KEY || ''
 
 const addressZ = z.string().regex(/^0x[a-fA-F0-9]{40}$/)
 
@@ -15,56 +14,6 @@ const schema = z.object({
   address: addressZ,
   userSignature: z.string(),
 })
-
-type ValidatorResponse = {
-    status: string;
-    data: Array<{
-      publickey: string;
-      validatorindex: number;
-    }>;
-  };
-  
-  type ExecutionResponse = {
-    status: string;
-    data: any; // Replace 'any' with the actual shape of the response
-  };
-  
-  async function fetchBeaconChainData(userAddress: string): Promise<any> {
-    try {
-      const validatorUrl = `https://beaconcha.in/api/v1/validator/withdrawalCredentials/${userAddress}?limit=10&offset=0&apikey=${BEACONCHAIN_API_KEY}`
-      const validatorRes = await fetch(validatorUrl, {
-        headers: {
-          'Accept': 'application/json',
-        },
-      })
-  
-      const validatorData: ValidatorResponse = await validatorRes.json();
-  
-      if (validatorData.status === 'OK' && validatorData.data.length > 0) {
-        const validatorIndex = validatorData.data[0].validatorindex;
-  
-        const executionUrl = `https://beaconcha.in/api/v1/execution/${validatorIndex}/produced?offset=0&limit=10&sort=desc&APIKEY=${BEACONCHAIN_API_KEY}`;
-
-        const executionRes = await fetch(executionUrl, {
-          headers: {
-            'Accept': 'application/json',
-          },
-        });
-  
-        const executionData: ExecutionResponse = await executionRes.json();
-  
-        if (executionData.status === 'OK') {
-          const executionInfo = executionData.data;
-          // Do something with executionInfo
-          console.log(executionInfo);
-          return executionInfo;
-        }
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
-  
   
   
 
