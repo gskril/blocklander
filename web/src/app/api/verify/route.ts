1abe210c2999990a726056052a3e126ceb201f6b
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-10-27 23:16:27 -0400

    Prettier

--- web/src/app/api/verify/route.ts
+++ web/src/app/api/verify/route.ts
@@ -2,8 +2,12 @@ import { NextRequest, NextResponse } from 'next/server'
 import z from 'zod'
 import { recoverAddress } from 'ethers/lib/utils'
 import { hashMessage } from '@ethersproject/hash'
-import { parseSearchParams, generateMintingSignature, Signature, fetchBeaconChainData } from '@/lib/utils'
-
+import {
+  parseSearchParams,
+  generateMintingSignature,
+  Signature,
+  fetchBeaconChainData,
+} from '@/lib/utils'
 
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || ''
 const TEST_CONTRACT_ADDRESS = process.env.TEST_CONTRACT_ADDRESS || ''
@@ -14,41 +18,47 @@ const schema = z.object({
   address: addressZ,
   userSignature: z.string(),
 })
-  
-  
 
 export async function GET(request: NextRequest) {
-    const params = parseSearchParams(request.nextUrl.searchParams)
-    const safeParse = schema.safeParse(params)
+  const params = parseSearchParams(request.nextUrl.searchParams)
+  const safeParse = schema.safeParse(params)
 
-    if (!safeParse.success) {
+  if (!safeParse.success) {
     return NextResponse.json(safeParse.error, { status: 400 })
+  }
+
+  // Access the validated data
+  const { address, userSignature } = safeParse.data
+
+  const projectSlug = 'blockProposer'
+  const network: string = 'sepolia'
+  const message = JSON.stringify({ network, projectSlug })
+  const signerAddress = addressZ.parse(
+    recoverAddress(hashMessage(message), userSignature)
+  )
+  if (address !== signerAddress) throw new Error('Invalid signer')
+
+  const executionData = await fetchBeaconChainData(address).catch((err) =>
+    console.error(err)
+  )
+
+  const contractAddress =
+    network === 'homestead' ? CONTRACT_ADDRESS : TEST_CONTRACT_ADDRESS
+
+  let signature: Signature | null = null
+
+  if (executionData) {
+    try {
+      signature = await generateMintingSignature(
+        address,
+        projectSlug,
+        contractAddress,
+        network
+      )
+    } catch (err) {
+      console.error(err)
     }
+  }
 
-    // Access the validated data
-    const { address, userSignature } = safeParse.data
-
-
-    const projectSlug = "blockProposer"
-    const network: string = "sepolia"
-    const message = JSON.stringify({ network, projectSlug })
-    const signerAddress = addressZ.parse(recoverAddress(hashMessage(message), userSignature))
-    if (address !== signerAddress) throw new Error('Invalid signer')
-
-    const executionData = await fetchBeaconChainData(address).catch(err => console.error(err));
-
-    const contractAddress = network === 'homestead' ? CONTRACT_ADDRESS : TEST_CONTRACT_ADDRESS
-
-    let signature: Signature | null = null
-
-    if(executionData) {
-        try {
-            signature = await generateMintingSignature(address, projectSlug, contractAddress, network)
-        } catch(err) {
-            console.error(err)
-        }
-
-    }
-
-    return NextResponse.json({ address, signature }, { status: 200 })
+  return NextResponse.json({ address, signature }, { status: 200 })
 }
--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -17,114 +17,124 @@ export function parseSearchParams(
   return query
 }
 
-
 export type Signature = {
-    r: `0x${string}`
-    s: `0x${string}`
-    _vs: string
-    recoveryParam: number
-    v: number
-    yParityAndS: string
-    compact: string
+  r: `0x${string}`
+  s: `0x${string}`
+  _vs: string
+  recoveryParam: number
+  v: number
+  yParityAndS: string
+  compact: string
 }
 
 export const createDomainSeparator = (
-    name: string,
-    contractAddress: string,
-    network: string,
-    tokenId = '1',
+  name: string,
+  contractAddress: string,
+  network: string,
+  tokenId = '1'
 ): string => {
-    // tokenId is use for 1155s, where each tokenId has different mint requirements.
-    // for 712s, tokenId is always 1.
+  // tokenId is use for 1155s, where each tokenId has different mint requirements.
+  // for 712s, tokenId is always 1.
 
-    const networkId = network === 'homestead' ? mainnet.id : sepolia.id
+  const networkId = network === 'homestead' ? mainnet.id : sepolia.id
 
-    const DOMAIN_SEPARATOR = ethers.utils.keccak256(
-        ethers.utils.defaultAbiCoder.encode(
-            ['bytes32', 'bytes32', 'uint256', 'address'],
-            [
-                ethers.utils.keccak256(ethers.utils.toUtf8Bytes(name)),
-                ethers.utils.keccak256(ethers.utils.toUtf8Bytes(tokenId)),
-                networkId,
-                contractAddress,
-            ],
-        ),
+  const DOMAIN_SEPARATOR = ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(
+      ['bytes32', 'bytes32', 'uint256', 'address'],
+      [
+        ethers.utils.keccak256(ethers.utils.toUtf8Bytes(name)),
+        ethers.utils.keccak256(ethers.utils.toUtf8Bytes(tokenId)),
+        networkId,
+        contractAddress,
+      ]
     )
+  )
 
-    return DOMAIN_SEPARATOR
+  return DOMAIN_SEPARATOR
 }
 
-export const generateSignature = async (address: string, domainSeparator: string) => {
-    const signer = new Wallet(VALIDATOR_PRIVATE_KEY)
+export const generateSignature = async (
+  address: string,
+  domainSeparator: string
+) => {
+  const signer = new Wallet(VALIDATOR_PRIVATE_KEY)
 
-    const payloadHash = ethers.utils.defaultAbiCoder.encode(['bytes32', 'address'], [domainSeparator, address])
-    const messageHash = ethers.utils.keccak256(payloadHash)
+  const payloadHash = ethers.utils.defaultAbiCoder.encode(
+    ['bytes32', 'address'],
+    [domainSeparator, address]
+  )
+  const messageHash = ethers.utils.keccak256(payloadHash)
 
-    const signedAddress = await signer.signMessage(ethers.utils.arrayify(messageHash))
-    const signature: ethers.Signature = ethers.utils.splitSignature(signedAddress)
-    return signature
+  const signedAddress = await signer.signMessage(
+    ethers.utils.arrayify(messageHash)
+  )
+  const signature: ethers.Signature = ethers.utils.splitSignature(signedAddress)
+  return signature
 }
 
 export const generateMintingSignature = async (
-    memberAddress: string,
-    projectSlug: string,
-    contractAddress: string,
-    network: string,
+  memberAddress: string,
+  projectSlug: string,
+  contractAddress: string,
+  network: string
 ): Promise<Signature> => {
-    const domainSeparator = createDomainSeparator(projectSlug, contractAddress, network)
-    const signature = await generateSignature(memberAddress, domainSeparator)
-    return signature as Signature
+  const domainSeparator = createDomainSeparator(
+    projectSlug,
+    contractAddress,
+    network
+  )
+  const signature = await generateSignature(memberAddress, domainSeparator)
+  return signature as Signature
 }
 
 // export const ioredisClient = new Redis(REDIS_URL);
 
-
 type ValidatorResponse = {
-    status: string;
-    data: Array<{
-      publickey: string;
-      validatorindex: number;
-    }>;
-  };
-  
-  type ExecutionResponse = {
-    status: string;
-    data: any; // Replace 'any' with the actual shape of the response
-  };
-  
-  export async function fetchBeaconChainData(userAddress: string): Promise<any> {
-    try {
-      const validatorUrl = `https://beaconcha.in/api/v1/validator/withdrawalCredentials/${userAddress}?limit=10&offset=0&apikey=${BEACONCHAIN_API_KEY}`
-      const validatorRes = await fetch(validatorUrl, {
+  status: string
+  data: Array<{
+    publickey: string
+    validatorindex: number
+  }>
+}
+
+type ExecutionResponse = {
+  status: string
+  data: any // Replace 'any' with the actual shape of the response
+}
+
+export async function fetchBeaconChainData(userAddress: string): Promise<any> {
+  try {
+    const validatorUrl = `https://beaconcha.in/api/v1/validator/withdrawalCredentials/${userAddress}?limit=10&offset=0&apikey=${BEACONCHAIN_API_KEY}`
+    const validatorRes = await fetch(validatorUrl, {
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+
+    const validatorData: ValidatorResponse = await validatorRes.json()
+    console.log(validatorData)
+
+    if (validatorData.status === 'OK' && validatorData.data.length > 0) {
+      const validatorIndex = validatorData.data[0].validatorindex
+
+      const executionUrl = `https://beaconcha.in/api/v1/execution/${validatorIndex}/produced?offset=0&limit=10&sort=desc&APIKEY=${BEACONCHAIN_API_KEY}`
+
+      const executionRes = await fetch(executionUrl, {
         headers: {
-          'Accept': 'application/json',
+          Accept: 'application/json',
         },
       })
 
-      const validatorData: ValidatorResponse = await validatorRes.json();
-      console.log(validatorData)
-  
-      if (validatorData.status === 'OK' && validatorData.data.length > 0) {
-        const validatorIndex = validatorData.data[0].validatorindex;
-  
-        const executionUrl = `https://beaconcha.in/api/v1/execution/${validatorIndex}/produced?offset=0&limit=10&sort=desc&APIKEY=${BEACONCHAIN_API_KEY}`;
+      const executionData: ExecutionResponse = await executionRes.json()
 
-        const executionRes = await fetch(executionUrl, {
-          headers: {
-            'Accept': 'application/json',
-          },
-        });
-  
-        const executionData: ExecutionResponse = await executionRes.json();
-  
-        if (executionData.status === 'OK') {
-          const executionInfo = executionData.data;
-          // Do something with executionInfo
-          console.log(executionInfo);
-          return executionInfo;
-        }
+      if (executionData.status === 'OK') {
+        const executionInfo = executionData.data
+        // Do something with executionInfo
+        console.log(executionInfo)
+        return executionInfo
       }
-    } catch (error) {
-        console.error('Error fetching data:', error);
     }
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
 }
