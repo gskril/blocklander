3f94b6109491e97335da9e9dcc7cda5605ce95b1
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-11-02 17:58:34 -0400

    Add sharing image

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,6 +5,9 @@ import { Client } from './client'
 export const metadata: Metadata = {
   title: 'BlockLander',
   description: 'A commemorative NFT for Ethereum block proposers',
+  openGraph: {
+    images: ['./sharing.jpg'],
+  },
 }
 
 export default function Home() {
